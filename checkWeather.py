f28c5ece8c296da027e4c87cb70504a13cd8b052
Sartaj Singh <55857498+sartaj96singh@users.noreply.github.com>
2020-05-17 18:56:15 -0700

    Update checkWeather.py
    Added the description  of the weather and added an ending message.

--- checkWeather.py
+++ checkWeather.py
@@ -1,14 +1,14 @@
 import requests
 
-api_key = ''
+api_key = '437d736c4e5d4065b5b03f812e731629'
 api_address='http://api.openweathermap.org/data/2.5/weather?appid=' + api_key + '&q='
 city = input('City Name :')
 url = api_address + city
 json_data = requests.get(url).json()
 format_add = json_data['main']['temp']
+description = json_data['weather'][0]['description']
 fahrenheit = (format_add * 9/5 - 459.67)
 
+print('It is going to be %s in %s' % (description, city))
 print('The current temp in %s is %.2f degree Fahrenheit' % (city, fahrenheit))
-
-
-  
+print('Enjoy this time!')
